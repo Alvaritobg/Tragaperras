eb742553277d38bf9bba11cf4bafb1383a30eafc
Alejandro Merino <alejandro.merino@easypaymentgateway.com>
2020-04-30 12:10:41 +0200

    Arreglo funcion comprobar

--- tragaperras.js
+++ tragaperras.js
@@ -2,6 +2,7 @@ var ruedas = 3;
 var figuras = ['herradura', 'campana', 'trebol', 'estrella', 'diamante',
     'cereza', 'siete', 'corona', 'sandia', 'fresa'
 ];
+var estadoRuedas = [];
 
 var monedas = 20;
 document.getElementById("monedas").innerHTML = `${monedas}`;
@@ -21,10 +22,11 @@ function tirar() {
 function tirarRueda(i) {
     let r1 = Math.floor(Math.random() * figuras.length);
     document.getElementById(`rueda${i}`).innerHTML = `<img src="img/${figuras[r1]}.png" alt="${figuras[r1]}">`;
+    estadoRuedas[i - 1] = figuras[r1];
 }
 
 function comprobar() {
-    if (r1 === r2 && r1 === r3 && r2 == r3) {
+    if (estadoRuedas[0] === estadoRuedas[1] && estadoRuedas[1] === estadoRuedas[2]) {
         alert("Premio");
         monedas = monedas + 10;
     }
