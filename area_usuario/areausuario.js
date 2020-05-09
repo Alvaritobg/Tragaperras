1d30c5420b53cf036c773926730a30e3c0aac492
Álvaro BG <62941163+Alvaritobg@users.noreply.github.com>
2020-05-09 21:37:34 +0200

    buscando error en fetch

--- area_usuario/areausuario.js
+++ area_usuario/areausuario.js
@@ -18,29 +18,27 @@ class IniAreausuario {
     return document.querySelector('.ultimosPostUsuario');
   }
 
-
   //funcion para manejar ajax call (Promesas)
   ajaxCall(url) {
     fetch(url)
       .then(data => data.json(data))
-      .then(data => this.generadorDatos.netx(data))
+      .then(data => this.generadorDatos.next(data))
       .catch(err => `Problemas obteniendo la información ${err}`);
   }
+
   //guarda y pintara la info obtenida de Json (method)
   pintorDatos(user, posts) {
-    //const { name, username } = user;
-    const pintarUserName = `<strong>${user}</strong>`;
-    const pintarPostTitulos = posts.map(post => `<li><i class="fas fa-caret-right"> ${post.title}</li>`);
+    const {name, username} = user;
+    const pintarUserName = `<strong>${username}</strong>`;
+    const pintarPostTitulos = posts.map(post => `<li><i class="fas fa-caret-right"> ${post.title}</li>`).join('');
       IniAreausuario.PINTAR_USER_NAME.innerHTML = pintarUserName;
       IniAreausuario.PINTAR_USER_POST.innerHTML = pintarPostTitulos;
-
-
   }
+
   // Funcion para realizar las llamadas de usuarios, post y comentarios (Generadores)
   *obtenerInfo(usuarioIdentificador) {
     const user = yield this.ajaxCall(`${IniAreausuario.URL_BASE}/users/${usuarioIdentificador}`);
     const posts = yield this.ajaxCall(`${IniAreausuario.URL_BASE}/posts?userId=${usuarioIdentificador}`);
-    // no funciona y no da error -> console.log(user, posts);
     this.pintorDatos(user, posts);
   }
   //Comprueba que lo introducido en el input es una id valida
